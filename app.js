5c69bfe01207fd40249c2e4488d222bb525f7be8
Joey Pagaduan <joey.paga220@gmail.com>
2023-06-06 11:27:30 -0700

    added cors middleware, starting api coding

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-require("dotenv").config()
-const express = require("express")
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
 // Setup your Middleware and API Router here
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+
+// cors to allow test access to server
+app.use(cors());
+// app.get('/products/:id', function (req, res, next) {
+//     res.json({msg: 'This is CORS-enabled for all origins!'})
+//   })
+  
+//   app.listen(80, function () {
+//     console.log('CORS-enabled web server listening on port 80')
+//   })
 
 // Import the API router
 const apiRouter = require('./api');
