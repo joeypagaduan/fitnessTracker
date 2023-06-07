409d8f4de15b6c700a2527258903ce3af3e89afe
trantuyen1411 <trantuyen1411@gmail.com>
2023-06-07 10:49:25 -0700

    api

--- app.js
+++ app.js
@@ -3,5 +3,19 @@ const express = require("express")
 const app = express()
 
 // Setup your Middleware and API Router here
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+// Import the API router
+const apiRouter = require('./api');
+
+// Have the server use your API router with prefix '/api'
+app.use('/api', apiRouter);
+
+
+
 
 module.exports = app;
