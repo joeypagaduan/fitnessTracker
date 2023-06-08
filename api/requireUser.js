8a8ed5bb3f3532b633548d6c9400b3d207d99a2f
Joey Pagaduan <joey.paga220@gmail.com>
2023-06-08 11:05:40 -0700

    passes GET POST PATCH in routines api

--- api/requireUser.js
+++ api/requireUser.js
@@ -1,8 +1,9 @@
 const requireUser = (req, res, next) => {
     if (!req.user) {
         next({
-            name: "MissingUserError",
-            message: "You must be logged in to perform this action"
+            error: "You are not logged in",
+            message: "You must be logged in to perform this action",
+            name: "MissingUserError"
         });
     } 
     
